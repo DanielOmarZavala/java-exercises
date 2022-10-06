aa3e039a92415ab238ef92fd2b884da8fef199b1
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-10-06 12:53:56 -0500

    Developer Assessment Practice

--- src/com/practice/java/AssessmentPractice.java
+++ src/com/practice/java/AssessmentPractice.java
@@ -4,26 +4,35 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.lang.Integer.parseInt;
+
 public class AssessmentPractice {
 
     public static void main(String[] args) {
 
         String[] arr = {"99 88 300", "3 23 80", "8 3 40"};
-        List<String> newArr1 = new ArrayList<>();
+//        String[] trimmedArr = new String[2];
+        List<String> newColl = new ArrayList<>();
 
-
-
-        System.out.println(newArr1);
-
+        System.out.println(newColl);
         System.out.println(Arrays.toString(arr));
 
-        for(int i = 0; i < arr.length; i++) {
 
-            System.out.println(arr[i]);
+        for (String s : arr) {
 
-            String[] newArr2 = arr[i].split(" ");
+            String[] trimmedArr = s.split(" ");
 
-            System.out.println(Arrays.toString(newArr2));
+//            System.out.println(arr[i].replaceAll("\\s", ""));
+
+            System.out.println(Arrays.toString(trimmedArr));
+
+            newColl.addAll(Arrays.asList(trimmedArr).subList(0, 2));
+
+            System.out.println(newColl);
+
+//            newColl[i] = arr[i].split(" ");
+
         }
+//        System.out.println(Arrays.toString(arr));
     }
 }
